1d3335f86f6c884b04283748635b0899108bba7f
rockwillz <37546115+rockwillz@users.noreply.github.com>
2018-04-13 07:01:20 -0700

    These are my project files.

---
+++ Figma/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="first">  INTERNSHIP 4.0: BACK AND BETTER </div>
+    <div class="second">   <?php
+    echo "The time is " . date("h:i:sa");
+    ?></div>
+    
+  
+    <div class="box text"> <center><p>find out more </p></center></div>
+    
+   
+</body>
+</html>
